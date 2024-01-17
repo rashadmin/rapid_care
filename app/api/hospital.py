b7f04cca4711e70c074b986d4f6457bd53c7b2e4
rashadmin <abdul.a.rasheed2021@gmail.com>
2024-01-17 13:19:53 +0100

    fixed bug

--- app/api/hospital.py
+++ app/api/hospital.py
@@ -6,7 +6,7 @@ from app import db
 
 @bp.route('/anony_users/<string:user_id>/hospital',methods=['GET'])
 def hospital_info_for_anony(user_id):
-    user_info = Anonyuser.query.filter_by(anony_user_id=user_id).to_dict()
+    user_info = Anonyuser.query.filter_by(username=user_id).to_dict()
     if '_links' in user_info:
         user_info.pop('_links')
     hospital_info = Conversation.query.filter_by(anony_user_id=user_id).first().to_hospital_dict()
