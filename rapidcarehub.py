b8db2422570c92de3201867ff0a9a8dbc1036f8e
rashadmin <abdul.a.rasheed2021@gmail.com>
2024-01-18 16:56:50 +0100

    added hospital counter

--- rapidcarehub.py
+++ rapidcarehub.py
@@ -3,7 +3,7 @@ from app.models import User,Conversation,Videos
 from app.search import add_to_index,query_index
 from app.videos.videos_functions import generate_other_names,return_url
 app = create_app()
-socketio.run(app,debug=False,port=8000,host='127.0.0.0')
+socketio.run(app,debug=False)
 @app.shell_context_processor
 def make_shell_context():
     return {'user':User,'conversation':Conversation,'db':db,'add':add_to_index,'query':query_index,'generate':generate_other_names,'vid':Videos,'get':return_url}
